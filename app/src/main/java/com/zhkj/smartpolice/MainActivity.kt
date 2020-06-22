f3df0a6d975beb35c71b680029d985091751a447
ZhangYe <zhangye98@foxmail.com>
2020-06-22 19:31:06 +0800

    智慧警保新建项目依赖ZyFrame

---
+++ app/src/main/java/com/zhkj/smartpolice/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.zhkj.smartpolice
+
+import android.view.View
+import com.sunny.zy.base.BaseActivity
+
+class MainActivity : BaseActivity() {
+
+    override fun setLayout(): Int = R.layout.activity_main
+
+    override fun initView() {
+
+    }
+
+    override fun loadData() {
+
+    }
+
+    override fun onClickEvent(view: View) {
+
+    }
+
+    override fun close() {
+
+    }
+}

b8f21967fa6105498b24229238b025d2f3f15e81
chenyongzuo <yongzuo.chen@foxmail.com>
2020-06-23 20:02:23 +0800

    个人中心页面开发

---
+++ app/src/main/java/com/zhkj/smartpolice/MineActivity.kt
@@ -0,0 +1,31 @@
+package com.zhkj.smartpolice
+
+import android.view.View
+import com.sunny.zy.base.BaseActivity
+
+/**
+ * Desc
+ * Author JoannChen
+ * Mail yongzuo.chen@foxmail.com
+ * Date 2020/6/23 15:39
+ */
+class MineActivity :BaseActivity() {
+
+    override fun setLayout(): Int  = R.layout.activity_mine
+
+    override fun initView() {
+
+    }
+
+    override fun loadData() {
+
+    }
+
+    override fun onClickEvent(view: View) {
+
+    }
+
+    override fun close() {
+
+    }
+}

6398ee3275c4bcc70b4322093a7e7220147a079f
chenyongzuo <yongzuo.chen@foxmail.com>
2020-06-28 18:47:15 +0800

    餐厅模块：订餐信息页面开发

---
+++ app/src/main/java/com/zhkj/smartpolice/meal/MealOrderInfoActivity.kt
@@ -0,0 +1,28 @@
+package com.zhkj.smartpolice.meal
+
+import android.view.View
+import com.sunny.zy.base.BaseActivity
+import com.zhkj.smartpolice.R
+
+class MealOrderInfoActivity : BaseActivity() {
+
+    override fun setLayout(): Int = R.layout.act_meal_order_info
+
+    override fun initView() {
+
+        defaultTitle("订餐信息")
+
+    }
+
+    override fun onClickEvent(view: View) {
+
+    }
+
+    override fun loadData() {
+
+    }
+
+    override fun close() {
+
+    }
+}

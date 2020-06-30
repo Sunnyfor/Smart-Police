464ee23c646c019e21f6368b2d83bff17fbf8900
ZhangYe <zhangye98@foxmail.com>
2020-06-30 15:33:56 +0800

    修改分页功能调用

--- app/src/main/java/com/zhkj/smartpolice/meal/MealOrderActivity.kt
+++ app/src/main/java/com/zhkj/smartpolice/meal/MealOrderActivity.kt
@@ -1,8 +1,7 @@
 package com.zhkj.smartpolice.meal
 
 import android.view.View
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.sunny.zy.activity.PullRefreshLayoutFragment
+import com.sunny.zy.activity.PullRefreshFragment
 import com.sunny.zy.base.BaseActivity
 import com.zhkj.smartpolice.R
 import com.zhkj.smartpolice.meal.adapter.MealOrderAdapter
@@ -11,15 +10,11 @@ import kotlinx.android.synthetic.main.act_meal_order.*
 
 class MealOrderActivity : BaseActivity() {
 
-    private val list = ArrayList<MealGoodsBean>()
-
     private val adapter: MealOrderAdapter by lazy {
-        MealOrderAdapter(list)
+        MealOrderAdapter(arrayListOf())
     }
 
-    private val pullRefreshLayoutFragment: PullRefreshLayoutFragment by lazy {
-        PullRefreshLayoutFragment()
-    }
+    private val pullRefreshLayoutFragment = PullRefreshFragment<MealGoodsBean>()
 
 
     override fun setLayout(): Int = R.layout.act_meal_order
@@ -27,12 +22,13 @@ class MealOrderActivity : BaseActivity() {
     override fun initView() {
 
         defaultTitle("购物车")
-        pullRefreshLayoutFragment.setLayoutManager(LinearLayoutManager(this))
         pullRefreshLayoutFragment.loadData = {
             loadData()
         }
-        pullRefreshLayoutFragment.setBaseRecyclerAdapter(adapter)
-        supportFragmentManager.beginTransaction().replace(fl_container.id, pullRefreshLayoutFragment).commit()
+        pullRefreshLayoutFragment.adapter = adapter
+
+        supportFragmentManager.beginTransaction()
+            .replace(fl_container.id, pullRefreshLayoutFragment).commit()
     }
 
     override fun onClickEvent(view: View) {
@@ -40,13 +36,12 @@ class MealOrderActivity : BaseActivity() {
     }
 
     override fun loadData() {
-
+        val list = ArrayList<MealGoodsBean>()
         list.add(MealGoodsBean("1", "酸汤肥牛", 0, 1, "好吃", "￥49.90"))
         list.add(MealGoodsBean("1", "酸菜鱼", 0, 1, "丫米", "￥39.90"))
         list.add(MealGoodsBean("1", "鱼香肉丝", 0, 1, "", "￥29.90"))
         list.add(MealGoodsBean("1", "宫保鸡丁", 0, 1, "", "￥29.90"))
         list.add(MealGoodsBean("1", "手撕包菜", 0, 1, "", "￥9.90"))
-
         pullRefreshLayoutFragment.addData(list)
     }
 

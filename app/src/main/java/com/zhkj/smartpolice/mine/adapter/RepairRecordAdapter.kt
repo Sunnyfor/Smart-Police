5b6e0bc6111d86937bac6f2788ebcf4cfa25baa0
chenyongzuo <yongzuo.chen@foxmail.com>
2020-07-15 18:27:12 +0800

    个人中心：维修记录空处理

--- app/src/main/java/com/zhkj/smartpolice/mine/adapter/RepairRecordAdapter.kt
+++ app/src/main/java/com/zhkj/smartpolice/mine/adapter/RepairRecordAdapter.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.sunny.zy.base.BaseRecycleAdapter
 import com.sunny.zy.base.BaseRecycleViewHolder
+import com.sunny.zy.utils.isStrEmpty
 import com.zhkj.smartpolice.R
 import com.zhkj.smartpolice.mine.bean.RepairRecordBean
 import kotlinx.android.synthetic.main.item_repair_record.view.*
@@ -17,12 +18,12 @@ class RepairRecordAdapter : BaseRecycleAdapter<RepairRecordBean>(arrayListOf()) 
 
     override fun onBindViewHolder(holder: BaseRecycleViewHolder, position: Int) {
         holder.itemView.tv_apply_title.text = getData(position).applyContent
-        holder.itemView.tv_money.text = ("￥ ${getData(position).money}")
+        holder.itemView.tv_money.text = ("￥ ${isStrEmpty(getData(position).money, "0")}")
         holder.itemView.tv_apply_name.text = ("申请人：${getData(position).petitioner}（${getData(position).petitionerPhone}）")
-        holder.itemView.tv_apply_time.text = ("申请时间：${getData(position).createTime}")
+        holder.itemView.tv_apply_time.text = ("申请时间：${isStrEmpty(getData(position).createTime, "暂无")}")
         holder.itemView.tv_order_number.text = ("订单号：${getData(position).attachmentGroupId ?: ""}")
-        holder.itemView.tv_order_time.text = ("( ${getData(position).repairRecordEntity?.createTime} )")
-        holder.itemView.tv_content.text = "${getData(position).repairRecordEntity?.content}"
+        holder.itemView.tv_order_time.text = ("( ${isStrEmpty(getData(position).repairRecordEntity?.createTime, "暂无")} )")
+        holder.itemView.tv_content.text = isStrEmpty(getData(position).repairRecordEntity?.content, "暂无")
     }
 
 }

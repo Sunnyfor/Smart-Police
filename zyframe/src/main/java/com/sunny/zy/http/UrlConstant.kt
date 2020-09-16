83213a4288cd3f0aef8c3b529d4227cefe02f712
chenyongzuo <yongzuo.chen@foxmail.com>
2020-09-16 19:37:58 +0800

    添加测试环境地址

--- zyframe/src/main/java/com/sunny/zy/http/UrlConstant.kt
+++ zyframe/src/main/java/com/sunny/zy/http/UrlConstant.kt
@@ -10,11 +10,10 @@ import com.sunny.zy.ZyFrameStore
  */
 object UrlConstant {
 
-//    const val host = "https://10.0.0.153:8807" // 内网测试地址
-
-//    const val host = "https://www.zhenhekj.com:8807" //正式地址
+//    const val host = "http://114.242.23.225:8086/" //正式地址
 
     const val IP = "122.51.69.116:8082" //测试环境
+//    const val IP = "114.242.23.225:8086"  //测试环境
 
 //    const val IP = "192.168.1.106:80" //演示接口
 
